623eb82a08924cafe6aa30d06c7dd29fcf62e7ea
Adeel Zafar <adeelzafar619@gmail.com>
2020-08-10 03:54:36 +0500

    Add resources

--- app/src/main/java/adeel/moviedb/ui/main/views/activities/DetailActivity.kt
+++ app/src/main/java/adeel/moviedb/ui/main/views/activities/DetailActivity.kt
@@ -1,12 +1,10 @@
 package adeel.moviedb.ui.main.views.activities
 
-import adeel.moviedb.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class CoroutinesSplashActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
     }
 }
---
+++ app/src/main/java/adeel/moviedb/ui/main/views/fragments/PopularMoviesFragment.kt
@@ -0,0 +1,10 @@
+package adeel.moviedb.ui.main.views.fragments
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class PopularMoviesFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        TODO("Not yet implemented")
+    }
+}
